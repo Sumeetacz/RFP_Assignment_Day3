0c841d22358bc7d0ede0037114db19a8bbba8618
Sumeetacz <sumeetacz@gmail.com>
2022-03-07 20:22:42 +0530

    Day3

---
+++ Assignment_1_Day3/Program.cs
@@ -0,0 +1,19 @@
+﻿			Console.WriteLine("Welcome to Line Comparison Computation Program");
+			int x1 = 2;
+			int x2 = 2;
+			int y1 = 3;
+			int y2 = 4;
+
+			int a1 = 2;
+			int a2 = 0;
+			int b1 = 3;
+			int b2 = 4;
+
+			double lgth1 = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+			Console.WriteLine("Length of first line is :" + lgth1);
+
+			double lgth2 = Math.Sqrt((a2 - a1) * (a2 - a1) + (b2 - b1) * (b2 - b1));
+			Console.WriteLine("Length of second line is :" + lgth2);
+		
+	
+
